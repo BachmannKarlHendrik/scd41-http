ccae6f2becdf63c93cd042941b3c746b9457f551
KarlHendrikBachmann <71638019+KarlHendrikBachmann@users.noreply.github.com>
2021-12-22 17:45:14 +0200

    local rest test

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 const char* ssid     = WIFINAME;
 const char* password = WIFIPASS;
+bool error = false;
 
 int LED_BUILTIN = 2;
 
@@ -34,32 +35,38 @@ void loop() {
   // put your main code here, to run repeatedly:
   //Send an HTTP POST request every 10 minutes
     //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
-      HTTPClient http;
-
-      String serverPath = "https://www.delfi.ee/";
-      
-      // Your Domain name with URL path or IP address with path
-      http.begin(serverPath.c_str());
-      
-      // Send HTTP GET request
-      int httpResponseCode = http.GET();
-      
-      if (httpResponseCode>0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        String payload = http.getString();
-        Serial.println(payload);
-      }
-      else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
-      }
-      // Free resources
-      http.end();
+    if(error) {
+        digitalWrite(LED_BUILTIN, HIGH);
     }
     else {
-      Serial.println("Request unsuccessful.");
+    if(WiFi.status()== WL_CONNECTED){
+        HTTPClient http;
+
+        String serverPath = "http://192.168.1.194/";
+        
+        // Your Domain name with URL path or IP address with path
+        http.begin(serverPath.c_str());
+        
+        // Send HTTP GET request
+        int httpResponseCode = http.GET();
+        
+        if (httpResponseCode>0) {
+          Serial.print("HTTP Response code: ");
+          Serial.println(httpResponseCode);
+          String payload = http.getString();
+          Serial.println(payload);
+        }
+        else {
+          Serial.print("Error code: ");
+          Serial.println(httpResponseCode);
+        }
+        // Free resources
+        http.end();
+      }
+      else {
+        Serial.println("Request unsuccessful.");
+        error = true;
+      }
+      delay(500);
     }
-    delay(500);
 }
